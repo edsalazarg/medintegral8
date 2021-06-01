e24e51947fcc73c353de0fbb32528302890c88a6
Jairo Gonzalez <zhaid.gonzalez@alumnos.udg.mx>
2021-06-01 00:46:02 -0500

    Added Spatie. Created initial seeders.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = Role::create(['name' => 'Admin']);
+        $role_user  = Role::create(['name' => 'User']);
+
+        Permission::create(['name' => 'Create user']);
+    }
+}
