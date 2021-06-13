a153b10a61bef9c10b45a5a5b6103c43ffd5396d
Jairo Gonzalez <zhaid.gonzalez@alumnos.udg.mx>
2021-06-13 15:19:33 -0500

    Fixed medical record index

--- resources/views/med_records/index.blade.php
+++ resources/views/med_records/index.blade.php
@@ -5,26 +5,47 @@
 @section('content')
 
     <h3>All medical records</h3>
-
     @if ($med_records->count())
+        <table>
         @foreach ($med_records as $med_record)
-            <div>
-                <h6>Patient</h6>
-                <span>{{$med_record->patient->firstName}}</span>
-                <h6>Date</h6>
-                <span>{{$med_record->created_at}}</span>
-                <form class="" action="{{route('med_records.destroy', $med_record->id)}}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-                <form class="" action="{{route('med_records.edit', $med_record->id)}}" method="get">
-                    @csrf
-                    <button type="submit">Edit</button>
-                </form>
-
-            </div>
+            <tr>
+                <td>
+                    {{$med_record->patient->firstName}} {{$med_record->patient->lastName}}
+                </td>
+                <td>
+                    {{$med_record->blood_group}}
+                </td>
+                <td>
+                    {{$med_record->diabetes}}
+                </td>
+                <td>
+                    {{$med_record->hypertension}}
+                </td>
+                <td>
+                    {{$med_record->epilepsy}}
+                </td>
+                <td>
+                    {{$med_record->asthma}}
+                </td>
+                <td>
+                    {{$med_record->pregnancies}}
+                </td>
+                <td>
+                    <form class="" action="{{route('med_records.destroy', $med_record->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+                <td>
+                    <form class="" action="{{route('med_records.edit', $med_record->id)}}" method="get">
+                        @csrf
+                        <button type="submit">Edit</button>
+                    </form>
+                </td>
+            </tr>
         @endforeach
+        </table>
     @else
         <p>Empty</p>
     @endif
