9bd485d3e84aa91087fc3c881ae30540a8725ce0
Ivan Moran <ivan_rdm75@hotmail.com>
2021-06-13 19:27:50 -0500

    Finished Show Patient view

--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -4,79 +4,114 @@
 
 @section('content')
 
-    <h1>{{ $patient['firstName'] }} {{ $patient['lastName'] }}</h1>
+{{--    <h1>{{ $patient['firstName'] }} {{ $patient['lastName'] }}</h1>--}}
 
-    <a href="{{route('patients.edit', ['patient' => $patient->id])}}">Edit</a>
+{{--    <a href="{{route('patients.edit', ['patient' => $patient->id])}}">Edit</a>--}}
 
-    <form action="{{route('patients.destroy', ['patient' => $patient->id])}}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="submit" value="DELETE">
-    </form>
+{{--    <form action="{{route('patients.destroy', ['patient' => $patient->id])}}" method="POST">--}}
+{{--        @csrf--}}
+{{--        @method('DELETE')--}}
+{{--        <input type="submit" value="DELETE">--}}
+{{--    </form>--}}
 
-    <p>Email: {{ $patient['email'] }}</p>
-    <p>Telephone: {{ $patient['telephone'] }}</p>
-    <p>Age: {{ $patient['age'] }}</p>
-    <p>Department: {{ $patient['department'] }}</p>
-    <p>Position: {{ $patient['position'] }}</p>
-    <p>Emergency Contact: {{ $patient['emergency_contact'] }}</p>
-    <p>Site: {{ $patient['site'] }}</p>
-    <p>Added {{$patient->created_at->diffForHumans()}}</p>
+{{--    <p>Email: {{ $patient['email'] }}</p>--}}
+{{--    <p>Telephone: {{ $patient['telephone'] }}</p>--}}
+{{--    <p>Age: {{ $patient['age'] }}</p>--}}
+{{--    <p>Department: {{ $patient['department'] }}</p>--}}
+{{--    <p>Position: {{ $patient['position'] }}</p>--}}
+{{--    <p>Emergency Contact: {{ $patient['emergency_contact'] }}</p>--}}
+{{--    <p>Site: {{ $patient['site'] }}</p>--}}
+{{--    <p>Added {{$patient->created_at->diffForHumans()}}</p>--}}
+<div class="card card-custom">
+    <div class="card-header flex-wrap border-0 pt-6 pb-0">
+        <div class="card-title">
+            <h3 class="card-label">Ver paciente</h3>
+        </div>
+    </div>
+    <div class="card-body">
+            @include('patients.partials.form')
+            <br>
+            <h3 class="card-label">Historial clinico</h3>
+        @if(empty($patient->medrecord))
+            <p>El paciente aun no tiene historial clinico</p>
+            <br>
+        @else
+            <br>
+            <div>
+                <span class="label label-xl label-info label-inline mr-2">Grupo sanguineo: {{$patient->medrecord->blood_group}}</span>
+            </div>
+            <br>
 
-    <h1>Medical Record</h1>
+            @if($patient->medrecord->diabetes == 1)
+                <span class="label label-danger label-pill label-inline mr-2">Diabetes: Si</span>
+            @else
+                <span class="label label-primary label-inline font-weight-boldest mr-2">Diabetes: No</span>
+            @endif
 
-    @if(empty($patient->medrecord))
-        <p>No Medical record for Patient</p>
-    @else
-        <p>Blood Group: {{$patient->medrecord->blood_group}}</p>
-        <p>Diabetes: {{$patient->medrecord->diabetes ? "Yes" : "No"}}</p>
-        <p>Hypertension: {{$patient->medrecord->hypertension ? "Yes" : "No"}}</p>
-        <p>Epilepsy: {{$patient->medrecord->epilepsy ? "Yes" : "No"}}</p>
-        <p>Asthma: {{$patient->medrecord->asthma ? "Yes" : "No"}}</p>
+            @if($patient->medrecord->hypertension == 1)
+                <span class="label label-danger label-pill label-inline mr-2">Hipertensión: Si</span>
+            @else
+                <span class="label label-primary label-inline font-weight-boldest mr-2">Hipertensión: No</span>
+            @endif
 
-        <h2> Surgeries </h2>
-        @forelse($patient->medrecord->surgeries as $surgery)
-            <hr>
-            <p>Date: {{$surgery->date}}</p>
-            <p>Surgery: {{$surgery->surgery}}</p>
+            @if($patient->medrecord->epilepsy == 1)
+                <span class="label label-danger label-pill label-inline mr-2">Epilepsia: Si</span>
+            @else
+                <span class="label label-primary label-inline font-weight-boldest mr-2">Epilepsia: No</span>
+            @endif
+
+            @if($patient->medrecord->asthma == 1)
+                <span class="label label-danger label-pill label-inline mr-2">Asma: Si</span>
+            @else
+                <span class="label label-primary label-inline font-weight-boldest mr-2">Asma: No</span>
+            @endif
+            <br>
+            <br>
+            <br>
+            <h3> Cirugías </h3>
+            @forelse($patient->medrecord->surgeries as $surgery)
+                <p>Fecha: {{$surgery->date}}</p>
+                <p>Cirugía: {{$surgery->surgery}}</p>
+            @empty
+                <p>El paciente no tiene cirugias</p>
+            @endforelse
+            <br>
+            <h3> Alergias </h3>
+            @forelse($patient->medrecord->allergies as $allergy)
+                <p>Alergia: {{$allergy->allergy}}</p>
+            @empty
+                <p>El paciente no tiene alergias</p>
+            @endforelse
+        @endif
+
+        <br>
+        <h3> Citas </h3>
+        @forelse($patient->appointment as $appointment)
+            <p>Fecha: {{$appointment->created_at->format('d/m/Y')}}</p>
+            <p>Lugar: {{$appointment->location}}</p>
+            <p>Transferencia: {{$appointment->transfer}}</p>
+            <p>Notas: {{$appointment->notes}}</p>
+            <p>Presión sanguinea: {{$appointment->blood_pressure}}</p>
+            <p>Ritmo cardiaco: {{$appointment->heart_rate}}</p>
+            <p>Factor reumatoide: {{$appointment->rheumatoid_factor}}</p>
+            <p>Temperatura: {{$appointment->temperature}}</p>
+            <p>Saturación de oxígeno: {{$appointment->sat_o2}}</p>
+            <p>Glucosa: {{$appointment->glucose}}</p>
+            <p>Glasgow: {{$appointment->glasgow}}</p>
+            <p>Neurológico: {{$appointment->neurologic}}</p>
+            <p>Cabeza: {{$appointment->head}}</p>
+            <p>Cuello: {{$appointment->neck}}</p>
+            <p>Cardiopulmonar: {{$appointment->cardiopulmonary}}</p>
+            <p>Abdomen: {{$appointment->abdomen}}</p>
+            <p>Extremidades: {{$appointment->extremities}}</p>
+            <p>Diagnóstico: {{$appointment->diagnosis}}</p>
+            <p>Condición: {{$appointment->condition}}</p>
+            <p>Tratamiento: {{$appointment->treatment}}</p>
+            <p>Pronóstico: {{$appointment->forecast}}</p>
         @empty
-            <p>No Surgeries for Patient!</p>
+            <p>El paciente no tiene citas</p>
         @endforelse
-
-        <h2> Allergies </h2>
-        @forelse($patient->medrecord->allergies as $allergy)
-            <hr>
-            <p>Allergy: {{$allergy->allergy}}</p>
-        @empty
-            <p>No Allergies for Patient!</p>
-        @endforelse
-    @endif
-
-    <h1> Appointments </h1>
-
-    @forelse($patient->appointment as $appointment)
-        <h2>Date: {{$appointment->created_at->format('d/m/Y')}}</h2>
-        <p>Location: {{$appointment->location}}</p>
-        <p>Transfer: {{$appointment->transfer}}</p>
-        <p>Notes: {{$appointment->notes}}</p>
-        <p>Blood Pressure: {{$appointment->blood_pressure}}</p>
-        <p>Heart Rate: {{$appointment->heart_rate}}</p>
-        <p>Rheumatoid Factor: {{$appointment->rheumatoid_factor}}</p>
-        <p>Temperature: {{$appointment->temperature}}</p>
-        <p>Oxygen Saturation: {{$appointment->sat_o2}}</p>
-        <p>Glucose: {{$appointment->glucose}}</p>
-        <p>Glasgow: {{$appointment->glasgow}}</p>
-        <p>Neurologic: {{$appointment->neurologic}}</p>
-        <p>Head: {{$appointment->head}}</p>
-        <p>Neck: {{$appointment->neck}}</p>
-        <p>Cardiopulmonary: {{$appointment->cardiopulmonary}}</p>
-        <p>Abdomen: {{$appointment->abdomen}}</p>
-        <p>Extremities: {{$appointment->extremities}}</p>
-        <p>Diagnosis: {{$appointment->diagnosis}}</p>
-        <p>Condition: {{$appointment->condition}}</p>
-        <p>Treatment: {{$appointment->treatment}}</p>
-        <p>Forecast: {{$appointment->forecast}}</p>
-    @empty
-        <p>No Appointments yet!</p>
-    @endforelse
+    </div>
+</div>
 @endsection
+
