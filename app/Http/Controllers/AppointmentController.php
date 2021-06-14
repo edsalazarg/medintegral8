5f54827d83b422979ba7fb50f34ef5cedfe1e54a
Jairo Gonzalez <zhaid.gonzalez@alumnos.udg.mx>
2021-06-14 11:00:26 -0500

    Blocked non registered users

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -8,6 +8,10 @@ use App\Models\Patient;
 
 class AppointmentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
     public function index()
     {
         $appointments = Appointment::all();
