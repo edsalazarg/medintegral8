dddc8b0b57acb1fe1d150e08f0b7bba31bc5eb5c
Eduardo Salazar <jesus.sgomez@alumnos.udg.mx>
2021-06-21 22:10:42 -0500

    Deleting title of appointments

--- resources/views/appointments/partials/form.blade.php
+++ resources/views/appointments/partials/form.blade.php
@@ -1,9 +1,4 @@
 <div class="card card-custom">
-    <div class="card-header flex-wrap border-0 pt-6 pb-0">
-        <div class="card-title">
-            <h3 class="card-label">Editar Cita</h3>
-        </div>
-    </div>
     <div class="card-body">
         <div class="form-group">
             @isset($patients)
