221e09fa45a2922252ca6c267dad6f77e6465e17
Eduardo Salazar <jesus.sgomez@alumnos.udg.mx>
2021-06-22 23:25:59 -0500

    Adding name to appointment index

--- public/js/appointmentsTable.js
+++ public/js/appointmentsTable.js
@@ -20,7 +20,7 @@ var KTDatatableHtmlTableDemo = function() {
             },
             columns: [
                 {
-                    field: 'Lugar',
+                    field: 'nombre',
                     width: 70,
                 },
             ],
